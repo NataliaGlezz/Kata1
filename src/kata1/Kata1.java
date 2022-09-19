10872425a41646e91cbc654b811703bf52655515
Natalia <Usuario@DESKTOP-2UUSSBD>
2022-09-19 22:24:40 +0100

    Tercera versión de Kata1

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,15 +1,12 @@
 package kata1;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Calendar date;
-        date = GregorianCalendar.getInstance();
-        date.set(2002,7,18);
+        LocalDate date;
+        date = LocalDate.of(2002,7,18);
         Person person = new Person("Natalia", date);
         System.out.println(person.getName() + " tiene " + person.getAge() + " años");
     }
